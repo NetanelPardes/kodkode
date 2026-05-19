792e5b53cc1be0f418a4ec48ca597f410fef268b
Netanel Pardes <sendi8475@gmail.com>
2026-05-19 12:43:28 +0300

    add  Scopes & Modules exrecise

---
+++ week_6/scopes_and_modules/scopes_and_modules.py
@@ -0,0 +1,40 @@
+# #ex1   
+# count = 0
+# def  bump():
+#     global count
+#     count += 1
+# def value():
+#     return count
+# bump()
+# bump()
+# bump()
+# print(value())
+
+# #ex2
+# def make_counter():
+#     count = 0
+#     def add_func():
+#         nonlocal count
+#         count += 1
+#         return count
+#     return add_func
+# c = make_counter()
+# print(c()) # 1
+# print(c()) # 2
+# print(c()) # 3
+
+# #ex3
+# x = "global"
+# def outer():
+#     x = "enclosing"
+#     def inner():
+#         x = "local"
+#         print(x)
+#     inner()
+#     print(x)
+# outer()
+# print(x)
+
+# #ex4
+# list = [1, 2, 3]
+# print(list(range(5)))
