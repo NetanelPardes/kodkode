1abbbf3c725f5655df6ba548352b367a467c319d
Netanel Pardes <sendi8475@gmail.com>
2026-06-08 16:52:17 +0300

    add a post raout for a update

--- week_9/sql/server_messages.py
+++ week_9/sql/server_messages.py
@@ -18,6 +18,27 @@ def get_all_messages():
     message = db_messages.get_all_messages()
     return {"message": message}
 
+@app.get("/messages/{message_id}", status_code=200)
+def get_message_by_id(message_id:int):
+    message = db_messages.get_message_by_id(message_id)
+    if not message:
+        raise HTTPException(status_code=404 ,detail="Message does not exist.")
+    return message
+
+@app.put("/messages/{message_id}",status_code=200)
+def update_message(message_id , new_data:dict):
+    message = db_messages.update_message(message_id , new_data)
+    if not message:
+        raise HTTPException(status_code=404 ,detail="Message does not exist.")
+    return message
+
+@app.delete("/messages/{message_id}",status_code=200)
+def delete_message(message_id):
+    message = db_messages.delete_message(message_id)
+    if not message:
+        raise HTTPException(status_code=404 ,detail="Message does not exist.")
+    return message
+
 @app.get("/messages/{classification}")
 def get_all_messages_by_classification(classification:str):
     messages_by_classification = db_messages.get_all_messages_by_classification(classification)
@@ -27,6 +48,7 @@ def get_all_messages_by_classification(classification:str):
 def new_message(new_message:dict):
     new_id = db_messages.create_message(new_message['unit'], new_message['classification'],new_message['content'], new_message[' source'])
     return {"status": "created", "id": new_id}
+
 
 
 
